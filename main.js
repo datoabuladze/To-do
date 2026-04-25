a4522a9021d1392f08d8a35446560ff4d3c7a323
datoabuladze <dato_abuladze@mail.ru>
2026-04-25 22:24:07 +0400

    mushaobs yvealferi, edit,save,remove

--- main.js
+++ main.js
@@ -2,14 +2,18 @@ function create() {
   let button = document.querySelector(".container .top button");
   let inp = document.querySelector(".container .top input");
   let bottom = document.querySelector(".container .bottom");
+  let editbutton = document.querySelector("editButton");
+  let editable = document.querySelector("editable");
 
   button.addEventListener("click", () => {
     if (inp.value) {
       let newItem = `
 <div class="inner">
-          <span>${inp.value}</span>
+          
+                  <input id="editable" type="text" value="${inp.value}" disabled />
+
           <div class="buttons">
-            <button>Edit</button>
+            <button  onclick="edit(this)" id="editButton">Edit</button>
             <button id="but">Remove</button>
           </div>
         </div>
@@ -17,6 +21,7 @@ function create() {
       bottom.innerHTML += newItem;
     }
     inp.value = "";
+
     let remove = document.getElementById("but");
     remove.addEventListener("click", () => {
       console.log(remove);
@@ -24,5 +29,15 @@ function create() {
       remove.parentElement.parentElement.remove();
     });
   });
+}
+function edit(btn) {
+  if (editable.disabled) {
+    editable.disabled = false;
+    editButton.innerText = "Save";
+    editable.focus();
+  } else {
+    editable.disabled = true; //
+    editButton.innerText = "Edit";
+  }
 }
 create();
